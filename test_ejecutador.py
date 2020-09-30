9dff666b56a8bb68535df842f1d5861a1225e4ae
Martin-Ruggeri-Bio <martinruggeri18@gmail.com>
2020-09-29 21:59:33 -0300

    arreglo11

---
+++ test_ejecutador.py
@@ -0,0 +1,10 @@
+from test_adivinanza import TestAdivinanza
+from test_ahorcado import TestAhorcado
+from test_jugador import TestJugador
+from test_palabra import TestPalabra
+
+
+primertest = TestJugador()
+segundotest = TestPalabra()
+tercertest = TestAhorcado()
+cuartotest = TestAdivinanza()
