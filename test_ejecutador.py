90891e36f97b45cda3141fab3ef0202aabdd5d29
Martin-Ruggeri-Bio <martinruggeri18@gmail.com>
2020-10-14 13:48:11 -0300

    test partida

--- test_ejecutador.py
+++ test_ejecutador.py
@@ -1,8 +1,6 @@
 from test_adivinanza import TestAdivinanza
-from test_ahorcado import TestAhorcado
 from test_partida import TestPartida
 
 
 segundotest = TestPartida()
-tercertest = TestAhorcado()
 cuartotest = TestAdivinanza()
